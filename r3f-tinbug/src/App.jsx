c4feb775965578843868f84d5f0d58a7719d929a
Sergi Bray <sergibpublic@gmail.com>
2025-01-29 23:17:35 +0000

    plays gt song

--- r3f-tinbug/src/App.jsx
+++ r3f-tinbug/src/App.jsx
@@ -3,17 +3,82 @@
 import { Canvas } from "@react-three/fiber";
 import { PerspectiveCamera, PositionalAudio, Text3D } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { create } from "zustand";
 
+const useStore = create((set) => ({
+  isPlaying: false,
+  selectedTrack: "0KoSzqJC4pDWNdZdqJhgA4",
+  playpause: () => set((state) => ({ isPlaying: !state.isPlaying })),
+}));
+
+const SpotifyIframe = ({ emitController = () => {} }) => {
+  // const bears = useBearStore((state) => state.bears){
+  const spotifyId = useStore((state) => state.selectedTrack);
+  const isPlaying = useStore((state) => state.isPlaying);
+  const type = "track";
+  // const spotifyId = "0KoSzqJC4pDWNdZdqJhgA4";
+
+  const spotifyScriptUrl = "https://open.spotify.com/embed/iframe-api/v1";
+
+  const [controller, setController] = useState(null);
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.setAttribute("id", "spotify-iframe-api");
+    script.src = spotifyScriptUrl;
+    document.body.appendChild(script);
+
+    script.onload = () => {
+      window.onSpotifyIframeApiReady = (IFrameAPI) => {
+        const element = document.getElementById("spotify-iframe-api");
+        const options = {
+          width: 200,
+          height: 0,
+        };
+        const callback = (EmbedController) => {
+          setController(EmbedController);
+        };
+        IFrameAPI.createController(element, options, callback);
+      };
+    };
+
+    return () => document.body.removeChild(script);
+  }, []);
+
+  useEffect(() => {
+    if (controller) {
+      controller.loadUri(`spotify:${type}:${spotifyId}`);
+
+      // Use the emit controller to pass the EmbedController
+      // to the parent component for more interactions
+      emitController(controller);
+    }
+  }, [controller, spotifyId, type]);
+  console.log(isPlaying);
+  console.log("isPlaying");
+  useEffect(() => {
+    if (controller) {
+      if (isPlaying) {
+        // controller.play()
+        controller.resume();
+      } else {
+        controller.pause();
+      }
+    }
+  }, [isPlaying]);
+
+  return <div id="spotify-iframe-api" />;
+};
 
 const CustomCube = ({ position, size, color }) => {
   const ref = useRef();
   const [isHovered, setIsHovered] = useState(false);
+  const playpause = useStore((state) => state.playpause);
+  const isPlaying = useStore((state) => state.isPlaying);
 
-  // delta is the difference in time betw current frame and the previous frame
   useFrame((state, delta) => {
     ref.current.rotation.x += delta;
-    // ref.current.position.z = Math.sin(state.clock.elapsedTime)*2
+    ref.current.position.z = Math.sin(state.clock.elapsedTime) * 2;
   });
   return (
     <mesh
@@ -21,7 +86,7 @@ const CustomCube = ({ position, size, color }) => {
       ref={ref}
       onPointerEnter={(event) => (event.stopPropagation(), setIsHovered(true))}
       onPointerLeave={() => setIsHovered(false)}
-      onClick={()=>console.log('sjdflksjdklfjskldfjls')}
+      onClick={playpause}
     >
       <boxGeometry args={size} />
       <meshStandardMaterial color={isHovered ? "orange" : "lightblue"} />
@@ -29,37 +94,27 @@ const CustomCube = ({ position, size, color }) => {
   );
 };
 
-// window.onSpotifyIframeApiReady = (IFrameAPI) => {
-//   const element = document.getElementById("embed-iframe");
-//   const options = {
-//     uri: "spotify:track:0KoSzqJC4pDWNdZdqJhgA4",
-//   };
-//   const callback = (EmbedController) => {
-//     document.querySelector(".play").addEventListener("click", () => {
-//       alert();
-//       console.log("sdfs");
-//       EmbedController.play();
-//     });
-//   };
-//   IFrameAPI.createController(element, options, callback);
-// };
-
 function App() {
   // const App = () => {
   // const config = { fov: 35, position: [0, -1, 2], far: 1000, near: 0.1 };
   return (
-    <Canvas>
-      <ambientLight intensity={0.7} />
-      <directionalLight position={[0, 0, 2]} />
-      <group>
-        <CustomCube position={[0, 0, 2]} color={"red"} size={[1, 1, 1]} />
-        {/* <CustomCube position={[0, 2, 2]} color={"red"} size={[1, 1, 1]} /> */}
-        {/* <CustomCube position={[-2, 0, 2]} color={"orange"} size={[1, 1, 1]} />
+    <>
+      <div style={{ display: "hidden" }}>
+        <SpotifyIframe />
+      </div>
+      <Canvas>
+        <ambientLight intensity={0.7} />
+        <directionalLight position={[0, 0, 2]} />
+        <group>
+          <CustomCube position={[0, 0, 2]} color={"red"} size={[1, 1, 1]} />
+          {/* <CustomCube position={[0, 2, 2]} color={"red"} size={[1, 1, 1]} /> */}
+          {/* <CustomCube position={[-2, 0, 2]} color={"orange"} size={[1, 1, 1]} />
         <CustomCube position={[2, -2, 2]} color={"hotpink"} size={[1, 1, 1]} />
         <CustomCube position={[0, -2, 2]} color={"hotpink"} size={[1, 1, 1]} />
         <CustomCube position={[-2, 2, 2]} color={"hotpink"} size={[1, 1, 1]} /> */}
-      </group>
-    </Canvas>
+        </group>
+      </Canvas>
+    </>
   );
 }
 
